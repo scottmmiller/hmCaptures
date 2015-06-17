4e537f34063b8934d835e39c91dc9d45641a3871
Scott Skoot Miller <smmiller.work@gmail.com>
2015-06-17 11:27:43 -0600

    herringbone border set; navbar links no underline/no color

---
+++ js/MainController.js
@@ -0,0 +1,7 @@
+angular.module("hmCaptures")
+
+  .controller('MainController', MainController, mainService);
+
+
+
+});
---
+++ js/mainService.js
@@ -0,0 +1,3 @@
+angular.module("hmCaptures")
+
+  .service("mainService", mainService);
