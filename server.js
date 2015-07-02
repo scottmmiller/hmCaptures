a5880f3fd21874a22e6bc6bfe309549bd7b0e433
Scott Skoot Miller <smmiller.work@gmail.com>
2015-07-01 21:32:07 -0600

    added server.js with LOTS of comments of trying to get 'live'; found StackOverflow about 'Connect/ServeStatic'; trying to get on iamskoot.com

--- server.js
+++ server.js
@@ -1,41 +1,81 @@
 "use strict";
 
-var Express = require("express");
-var BodyParser = require("body-parser");
-var Passport = require("passport");
-var Mongoose = require("mongoose");
-var Session = require("express-session");
+//    NPM CONNECT
+var connect = require("connect");
+var serveStatic = require("serve-static");
+var http = require("http");
 
-var app = Express();
+var app = connect();
+
 var port = process.env.HMCAPTURES_PORT || 9999;
-  console.log("EXPRESS_PORT: " + port)
-var mongoUri = "";
+app.use(serveStatic(__dirname)).listen(port);
+console.log("Server running on: " + port);
 
-//    *****MIDDLEWARE*****
-app.use(Session ({
-  secret: "hmCaptures",
-  resave: true,
-  saveUninitialized: true
-}));
+// //  JOE EAMES: PLURALSIGHT TUTORIAL
+// var Express = require("express");
+//
+// var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+//
+// var app = Express();
+//
+// // app.set('views', __dirname + '/views');
+// // app.set('view engine', 'html');
+//
+// app.get("*", function(req, res) {
+//   // res.render('index');
+// });
+//
+// var port = 9999;
+// app.listen(port, function() {
+//   console.log("Server running on: " + port);
+// });
+//
+// //   *********************
 
-//    *****STRATEGIES*****
-
-//    *****CONNECTIONS*****
-Mongoose.connect(mongoUri, function() {
-  console.log("DB connected on " + mongoUri)
-})
-
-app.listen(port, function() {
-  if(port === process.env.EXPRESS_PORT) {
-    console.log("Listening on EXPRESS_PORT");
-  } else {
-    console.log("Listening on 9999")
-  }
-})
-
+//    NODE.JS Main Page Example
 // var http = require("http");
-//
 // http.createServer(function(req, res) {
+//   res.writeHead(200, {'Content-Type': 'text/html'});
+//   res.end(index);
+// }).listen(9999, "127.0.0.1");
+// console.log("Server is running");
+
+// var Express = require("express");
+// var BodyParser = require("body-parser");
+// var Passport = require("passport");
+// var Mongoose = require("mongoose");
+// var Session = require("express-session");
 //
+// var app = Express();
+// var port = process.env.HMCAPTURES_PORT || 9999;
+//   console.log("EXPRESS_PORT: " + port)
+// var mongoUri = "";
 //
-// }).listen(80);
+// //    *****MIDDLEWARE*****
+// app.use(Session ({
+//   secret: "hmCaptures",
+//   resave: true,
+//   saveUninitialized: true
+// }));
+//
+// //    *****STRATEGIES*****
+//
+// //    *****CONNECTIONS*****
+// Mongoose.connect(mongoUri, function() {
+//   console.log("DB connected on " + mongoUri)
+// })
+//
+// app.listen(port, function() {
+//   if(port === process.env.EXPRESS_PORT) {
+//     console.log("Listening on EXPRESS_PORT");
+//   } else {
+//     console.log("Listening on 9999")
+//   }
+// })
+//
+// // var http = require("http");
+// //
+// // http.createServer(function(req, res) {
+// //
+// //
+// // }).listen(80);
